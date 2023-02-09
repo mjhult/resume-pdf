5c15bc34d35afa0cf8705b66a8ed534dcf9ac3b4
Mathew Hultquist <mathew@gates.services>
2023-02-09 18:25:45 -0500

    Added more logs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import fs from "fs";
-import { render } from "./render";
+import fs from 'fs';
+import { render } from './render';
 
 (async () => {
-  console.log("Checking for pdf directory...");
-  console.time("Directory check");
-  if (!fs.existsSync(`${__dirname}/../build/pdf/`))
-    fs.mkdirSync(`${__dirname}/../build/pdf/`, { recursive: true });
-  console.timeEnd("Directory check");
+  console.time('Build');
+  try {
+    console.log('Checking for pdf directory...');
+    console.time('Directory check');
+    if (!fs.existsSync(`${__dirname}/../build/pdf/`))
+      fs.mkdirSync(`${__dirname}/../build/pdf/`, { recursive: true });
+    console.timeEnd('Directory check');
 
-  console.log("\nRendering PDF...");
-  console.time("Rendering");
-  await render();
-  console.timeEnd("Rendering");
+    console.log('\nRendering PDF...');
+    console.time('Rendering');
+    await render();
+    console.timeEnd('Rendering');
+    console.log('\nBuild complete ✅');
+  } catch (e) {
+    console.error(`🛑 An Error Ocurred. 🛑\n${e}`);
+  }
+  console.timeEnd('Build');
 })();

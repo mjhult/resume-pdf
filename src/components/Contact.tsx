c656bb4e51de979672c70bd99ed297152c330c3e
Mathew Hultquist <mathew@gates.services>
2023-02-09 15:13:14 -0500

    Added contact component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,26 @@
+import ReactPDF, { View, Text, StyleSheet } from '@react-pdf/renderer';
+import React, { FC } from 'react';
+import { textFormat } from '../helpers/helper';
+import { ContactInfo } from '../types/data';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+
+  text: {
+    marginLeft: 4,
+  },
+});
+
+export const Contact: FC<
+  ContactInfo & {
+    textStyle?: ReactPDF.Styles;
+    containerStyle?: ReactPDF.Styles;
+  }
+> = ({ type, data, icon, textStyle, containerStyle }) => (
+  <View style={{ ...styles.container, ...containerStyle }}>
+    {icon ?? <Text>{textFormat(type)}:</Text>}
+    <Text style={{ ...styles.text, ...textStyle }}>{data}</Text>
+  </View>
+);

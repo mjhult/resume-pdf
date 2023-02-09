442de53e48ddfb3a705d678282b538ff5be13c63
Mathew Hultquist <mathew@gates.services>
2023-02-09 18:26:45 -0500

    Style changes and formatting changes

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,32 +1,30 @@
 import React, { FC } from 'react';
 import { StyleSheet, Text, View } from '@react-pdf/renderer';
 import { WorkExperience } from '../types/data';
+import { colors } from '../data/data';
 
 const styles = StyleSheet.create({
   experienceContainer: {
-    // backgroundColor: 'red',
-    // width: 250,
-    // height: 150,
-    marginTop: 8,
-    // marginBottom: 8,
+    marginBottom: 12,
+    width: 230,
   },
 
   experienceTitle: {
-    fontSize: 18,
+    fontSize: 15,
     fontWeight: 'bold',
+    width: '100%',
   },
 
   experienceSubTitle: {
-    fontSize: 13,
+    fontSize: 10,
     fontWeight: 'normal',
+    width: '100%',
   },
 
   dutyTitle: {
     fontSize: 9,
-  },
-
-  dutySubtitle: {
-    fontSize: 8,
+    width: '100%',
+    marginTop: 3,
   },
 });
 
@@ -34,18 +32,26 @@ export const Experience: FC<WorkExperience> = ({
   title,
   subTitle,
   duties,
-  subTitleTextColor,
-  titleTextColor,
+  subTitleStyle,
 }) => {
   return (
     <View style={styles.experienceContainer}>
       <Text style={styles.experienceTitle}>{title}</Text>
-      <Text style={styles.experienceSubTitle}>{subTitle ?? null}</Text>
+      <Text style={[styles.experienceSubTitle, subTitleStyle]}>
+        {subTitle ?? null}
+      </Text>
       {duties?.map((duty, i) => (
         <Text
           key={i}
-          style={{ color: duty.textColor ?? '', ...styles.dutyTitle }}>
-          <Text style={{ color: duty.pointColor ?? '' }}>{duty.point}</Text>{' '}
+          style={[{ color: duty.textColor ?? '' }, styles.dutyTitle]}>
+          <Text
+            style={{
+              color: duty.pointColor ?? colors.primaryColor,
+              display: duty.point ? 'flex' : 'none',
+            }}>
+            {duty.point}
+            {duty.point ? ' ' : ''}
+          </Text>
           {duty.title}
         </Text>
       ))}
